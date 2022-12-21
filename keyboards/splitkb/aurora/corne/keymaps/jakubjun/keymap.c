13c5eb306db3e638f1c5c824084ce1de459ea316
Jun, Jakub <jakub.jun@firma.seznam.cz>
2022-12-21 12:08:37 +0100

    Formatting

--- keyboards/splitkb/aurora/corne/keymaps/jakubjun/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/jakubjun/keymap.c
@@ -15,10 +15,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [5] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO),
     [6] = LAYOUT_split_3x6_3(KC_NO, LCTL(KC_BSPC), KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO, LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), KC_NO, KC_NO, KC_NO, KC_BSPC, KC_BTN3, KC_BTN2, KC_BTN1, KC_NO, KC_NO, LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_0), KC_NO, KC_NO, TG(6), KC_NO, KC_NO, KC_WH_D, KC_NO, KC_NO, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, MO(5), MO(7), KC_NO),
     [7] = LAYOUT_split_3x6_3(KC_NO, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO, SGUI(KC_7), SGUI(KC_8), SGUI(KC_9), KC_NO, KC_NO, KC_NO, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_NO, SGUI(KC_4), SGUI(KC_5), SGUI(KC_6), SGUI(KC_0), KC_NO, KC_NO, RGB_RMOD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, SGUI(KC_1), SGUI(KC_2), SGUI(KC_3), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO)
-    /* [0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC, KC_LGUI, MO(1), KC_SPC, KC_ENT, MO(2), KC_RALT), */
-    /* [1] = LAYOUT_split_3x6_3(KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, _______, KC_SPC, KC_ENT, MO(3), KC_RALT), */
-    /* [2] = LAYOUT_split_3x6_3(KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, KC_LGUI, MO(3), KC_SPC, KC_ENT, _______, KC_RALT), */
-    /* [3] = LAYOUT_split_3x6_3(QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, _______, KC_SPC, KC_ENT, _______, KC_RALT) */
 };
 
 #ifdef ENCODER_ENABLE
@@ -90,12 +86,12 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 break;
             default:
                 if (clockwise) {
-                        tap_code16(LSFT(KC_N));
-                    } else {
-                        tap_code16(KC_N);
-                    }
+                    tap_code16(LSFT(KC_N));
+                } else {
+                    tap_code16(KC_N);
                 }
         }
+    }
 
     return false;
 }
@@ -137,39 +133,28 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("4:wm shift dir\n"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR(""), false);
-                /* oled_write_ln_P(PSTR("[_][_][_]"), false); */
-                /* oled_write_ln_P(PSTR("[sft][3][incr]"), false); */
                 break;
             case 5:
                 oled_write_ln_P(PSTR("5:fun\n"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR(""), false);
-                /* oled_write_ln_P(PSTR("[_][_][_]"), false); */
-                /* oled_write_ln_P(PSTR("[sft][3][incr]"), false); */
                 break;
             case 6:
                 oled_write_ln_P(PSTR("6:wm move num\n"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR(""), false);
-                /* oled_write_ln_P(PSTR("[_][_][_]"), false); */
-                /* oled_write_ln_P(PSTR("[sft][3][incr]"), false); */
                 break;
             case 7:
                 oled_write_ln_P(PSTR("7:rgb, wm shift num\n"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR(""), false);
-                /* oled_write_ln_P(PSTR("[_][_][_]"), false); */
-                /* oled_write_ln_P(PSTR("[sft][3][incr]"), false); */
                 break;
             case 8:
                 oled_write_ln_P(PSTR("8\n"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR(""), false);
-                /* oled_write_ln_P(PSTR("[_][_][_]"), false); */
-                /* oled_write_ln_P(PSTR("[sft][3][incr]"), false); */
                 break;
             default:
-                // Or use the write_ln shortcut over adding '\n' to the end of your string
                 oled_write_ln_P(PSTR("Undefined"), false);
         }
     }
