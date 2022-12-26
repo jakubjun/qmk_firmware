4c856a848e5aa5897d4e811fb63f5bd469eef851
Jun, Jakub <jakub.jun@firma.seznam.cz>
2022-12-26 21:53:50 +0100

    Add cool shit to other half

--- keyboards/splitkb/aurora/corne/config.h
+++ keyboards/splitkb/aurora/corne/config.h
@@ -18,5 +18,5 @@
 
 // Not yet available in `info.json`
 #ifdef OLED_ENABLE
-#   define OLED_FONT_H "keyboards/splitkb/aurora/corne/glcdfont.c"
+/* #   define OLED_FONT_H "keyboards/splitkb/aurora/corne/glcdfont.c" */
 #endif
